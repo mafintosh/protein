106148d12b2e700f8b8131749467aab83f82da6e
Mathias Buus <mathiasbuus@gmail.com>
2012-04-24 21:22:47 +0200

    added route test

---
+++ tests/route.js
@@ -0,0 +1,56 @@
+var protein = require('../index');
+var assert = require('assert');
+
+var URL = '/myurl/more';
+var complete = false;
+
+var fn = protein()
+	.use(function(req, res, next) {
+		assert.equal(req.url, URL);
+		next();
+	})
+	.use('/', function(req, res, next) {
+		assert.equal(req.url, URL);
+		next();
+	})
+	.use('/myurl', function(req, res, next) {
+		assert.equal(req.url, '/more');
+		next();
+	})
+	.use(function(req, res, next) {
+		assert.equal(req.url, URL);
+		next();
+	})
+	.use('/myurl', function(req, res, next) {
+		assert.equal(req.url, '/more');
+		process.nextTick(function() {
+			next();
+		});
+	})
+	.use(function(req, res, next) {
+		assert.equal(req.url, URL);
+		next();
+	})
+	.use('/notmyurl', function(req, res, next) {
+		assert.ok(false);
+	})
+	.use(function(req, res, next) {
+		assert.equal(req.url, URL);
+		next();
+	})
+	.use('/myurl/', function(req, res, next) {
+		assert.equal(req.url, '/more');
+		next();
+	})
+	.use(function(req, res) {
+		complete = true;
+		assert.equal(req.url, URL);
+	});
+
+fn({url:URL}, {}, function(err) {
+	throw err;
+});
+
+process.nextTick(function() {
+	assert.ok(complete);
+});
